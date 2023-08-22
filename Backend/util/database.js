ea1c32d826b5f619cd969577fac0954092b2ce84
roshinys <roshinchaya@gmail.com>
2023-08-22 08:03:07 +0530

    configured env file

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("expensetracker", "root", "433842", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  "expensetracker",
+  process.env.DBUSER,
+  process.env.DBPASS,
+  {
+    host: process.env.DBHOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
